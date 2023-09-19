df48b91662ce94d375b233b38ee197bbe4b47ced
Kuesung Park <gueit214@naver.com>
2023-09-19 16:42:03 +0900

    feat : Headless - Compound Component 패턴

--- src/app/headless/page.tsx
+++ src/app/headless/page.tsx
@@ -1,17 +1,32 @@
 "use client";
-import CheckboxHeadless from "@/components/CheckboxHeadless";
+import CheckboxHeadless from "@/app/headless/components/CheckboxHeadless";
+import { useState } from "react";
+import CheckboxWrapper from "./components/CheckboxWrapper";
 
-export default async function page() {
+export default function page() {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
-    <CheckboxHeadless>
-      {({ isChecked, onChange }) => {
-        return (
-          <label>
-            <input type="checkbox" checked={isChecked} onChange={onChange} />
-            <span>체크박스</span>
-          </label>
-        );
-      }}
-    </CheckboxHeadless>
+    <div>
+      <CheckboxHeadless>
+        {({ isChecked, onChange }) => {
+          return (
+            <label>
+              <input type="checkbox" checked={isChecked} onChange={onChange} />
+              <span>체크박스</span>
+            </label>
+          );
+        }}
+      </CheckboxHeadless>
+
+      <CheckboxWrapper
+        id="checkbox-1"
+        isChecked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      >
+        <CheckboxWrapper.Checkbox />
+        <CheckboxWrapper.Label>체크박스 만들기</CheckboxWrapper.Label>
+      </CheckboxWrapper>
+    </div>
   );
 }
